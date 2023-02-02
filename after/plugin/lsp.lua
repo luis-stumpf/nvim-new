44d9890839192bd0a3261cd756b6777c5630f66d
Luis <luis@localhost.localdomain>
2023-02-02 17:07:01 +0100

    first commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,50 @@
+-- Learn the keybindings, see :help lsp-zero-keybindings
+-- Learn to configure LSP servers, see :help lsp-zero-api-showcase
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+lsp.ensure_installed({
+	'tsserver',
+	'sumneko_lua',
+	'rust_analyzer',
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' }
+			}
+		}
+	}
+})
+
+
+lsp.set_preferences({
+	suggest_lsp_servers = false,
+	sign_icons = {
+		error = 'E',
+		warn = 'W',
+		hint = 'H',
+		info = 'I'
+	}
+})
+
+
+lsp.on_attach(function(client, bufnr)
+	local opts = {buffer = bufnr, remap = false}
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+end)
+
+
+lsp.setup()
+
+vim.diagnostic.config({
+	virtual_text = true
+})
+
---
+++ lua/luis/init.lua
@@ -0,0 +1,2 @@
+require("luis.remap")
+require("luis.set")
