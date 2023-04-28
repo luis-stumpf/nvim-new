f32140840b3ad9fbc6ef07eee9721613d3a21e40
Luis Stumpf <trumpf25lp@gmail.com>
2023-04-28 23:12:58 +0200

    fixasdf: test fix

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -57,7 +57,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.formatting.commitlint,
+    null_ls.builtins.diagnostics.commitlint,
   },
 })
 
